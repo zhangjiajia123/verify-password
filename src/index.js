466161d2947607d91b0d4944f520f8538bd3a34c
panyong1209 <18710452575>
2023-02-14 14:03:28 +0800

    默认导出

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default verifyPassword = function(password) {
+export default function verifyPassword(password) {
   const regex = "/(?!^[0-9]{16,12}$)(?!^[A-Z]{6,12}$)(?!^[a-z]{6,12}$)^[0-9A-Za-z]{6,12}$/"
   return regex.test(password)
 }
